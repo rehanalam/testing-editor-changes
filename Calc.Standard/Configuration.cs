fb564836f6cc6c38c0a6c24f5eb25d8e6a1fe87a
Unknown <git@apimatic.io>
2019-05-28 09:24:55 +0000

    sasadasda

---
+++ Calc.Standard/Configuration.cs
@@ -0,0 +1,12 @@
+using Calc.Standard.Models;
+namespace Calc.Standard
+{
+    public partial class Configuration
+    {
+
+
+        //The base Uri for API calls
+        public static string BaseUri = "http://examples.apimatic.io/apps/calculator";
+
+    }
+}
